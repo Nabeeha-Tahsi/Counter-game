8718659679db8d1314b0dfd2ce56a94881b2b69a
Nabeeha-Tahsi <munbsmmu@gmail.com>
2025-06-13 18:04:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let counter = document.getElementById('counter')
+let count = 0
+
+function increment(){
+    count++
+    counter.innerHTML = count
+}
+
+function decrement(){
+    
+    if(count > 0){
+        count--
+        counter.innerHTML = count
+    }else{
+        counter.innerHTML = 0
+    }
+    
+}
